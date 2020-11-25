32d738d785bd3f55409cb67abad492e23a137221
minhye <45410726+Choo-Minhye@users.noreply.github.com>
2020-11-25 14:53:10 +0900

    Add files via upload

---
+++ Mega9250.ino
@@ -0,0 +1,58 @@
+const unsigned long IMU_FREQUENCY = 50;
+const unsigned long SONAR_1_FREQUENCY = 15;
+const unsigned long BARO_FREQUENCY = 14;
+
+#include "Mega9250.h"
+
+void setup() {
+  SPI.begin();
+  IMU.begin();
+  
+  int IMU_gyroStatus = IMU.calibrateGyro();
+  int IMU_setAccelRange = IMU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
+
+  int IMU_accelStatus = IMU.calibrateAccel();
+  int IMU_magStatus = IMU.calibrateMag();
+
+
+  nh.initNode();
+  nh.subscribe(sub);
+
+  //  broadcaster.init(nh);
+  nh.advertise(pub);
+  nh.advertise(pub_baro);
+  baro.setReference();
+
+  nh.advertise(pub_range0);
+  nh.advertise(pub_range1);
+  
+  mc.addMotor(new PairedBaseMotor(LINEAR_ACTUATOR_L1, LINEAR_ACTUATOR_L2, LINEAR_ACTUATOR_R1, LINEAR_ACTUATOR_R2));
+  mc.addMotor(new BaseMotor(CONVEYOR_PIN1, CONVEYOR_PIN2));
+  mc.addMotor(new BaseMotor(ROLLSHUTTER_PIN1, ROLLSHUTTER_PIN2));
+
+  nh.advertiseService(light_server);
+  nh.advertiseService(ir_server);
+
+  nh.advertise(pub_range0);
+  nh.advertise(pub_range1);
+ 
+
+  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg.field_of_view = 0.6;  // fake
+  range_msg.min_range = 0.15;
+  range_msg.max_range = 1.5;
+
+
+}
+
+int count_IMU;
+long range_time_imu, range_time_baro;
+
+void loop()
+{
+  pub_IMU();
+  pub_Baro();
+  pub_Sonar();
+  nh.spinOnce();
+
+}
